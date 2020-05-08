98b3b059c326859ec7ab8db47eb1efbe46ee44a1
Pablo Melo <pablomelodev@gmail.com>
2020-05-07 23:36:38 -0300

    Private routes

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -22,12 +22,14 @@ interface AuthContextData {
   user: object;
   signIn(credentials: signInCredentials): Promise<void>;
   signOut(): void;
+  loading: boolean;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
@@ -39,7 +41,11 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+
+      setLoading(false);
     }
+
+    loadStorageData();
   }, []);
 
   const signIn = useCallback(async ({ email, password }) => {
@@ -65,7 +71,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut, loading }}>
       {children}
     </AuthContext.Provider>
   );
