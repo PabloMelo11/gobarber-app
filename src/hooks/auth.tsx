f86d9516a82d871eb05f8dd054b250f0937b7c6c
Pablo Melo <pablomelodev@gmail.com>
2020-06-17 00:36:14 -0300

    listing providers

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,9 +8,15 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface signInCredentials {
@@ -19,7 +25,7 @@ interface signInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: signInCredentials): Promise<void>;
   signOut(): void;
   loading: boolean;
@@ -39,6 +45,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -60,6 +68,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
